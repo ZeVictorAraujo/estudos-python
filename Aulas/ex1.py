c69e3f24b0619f9a69233fd6633b0ec940978126
ZeVictorAraujo <josearaujo.ba@hotmail.com>
2025-09-01 23:39:45 -0300

    Ex1: Peso ideal

--- Aulas/ex1.py
+++ Aulas/ex1.py
@@ -1,6 +1,23 @@
-idade = int(input("Digite sua idade: "))
 
-if idade >= 18:
-    print("Você é maior de idade.")
-else:
-    print("Você é menor de idade.")
+nome = input("Seu nome: ")
+
+while True:
+    sexo = str(input("Seu sexo: "))
+
+    if(sexo.upper() == "M") | (sexo.upper() == "F"):
+        
+        altura = float(input("Sua altura: "))
+        if (sexo.upper() == "M"):
+            peso_ideal = (72.7 * altura) - 58
+        elif (sexo.upper() == "F"):
+            peso_ideal = (62.1 * altura) - 44.7
+
+        print("Seu peso ideal é: ", peso_ideal, "Sexo: ", sexo)
+        break
+    else:
+        print("Informação incorreta. Use M ou F")
+        print("  ")
+
+
+
+        
